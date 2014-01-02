5f5756b7e745b73a39a6663de6eab5de143c0b1a
Nicolas <dughetti@zesmind.com>
2014-01-02 09:29:44 -0300

    Se simplifica Calculo.js
    Se reescribió el archivo Calculo.js, manteniendo el viejo y se creo un
    archivo para hacer las pruebas (test.js)

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ config.secret = '21686c96b6520c60c452ebc9742b3f4d30382d97a80a96b298886b3fd986dbc
 //Configuracion para operar
 config.pair = 'btc_usd';
 config.seconds = 10800;
+config.periodo = 10;
+config.candlesCount = 200;
 module.exports = config;
